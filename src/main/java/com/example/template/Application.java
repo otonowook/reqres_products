b82d9d5826e809b9563a905ec72a27665af7e705
otonowook <152375871+otonowook@users.noreply.github.com>
2024-06-20 17:15:16 +0900

    Update Application.java

--- src/main/java/com/example/template/Application.java
+++ src/main/java/com/example/template/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
+ 
     protected static ApplicationContext applicationContext;
     public static void main(String[] args) {
         applicationContext = SpringApplication.run(Application.class, args);
