c2e5ff50625fd1ab15c8d4eca7eb1a7446d18618
otonowook <152375871+otonowook@users.noreply.github.com>
2024-06-21 10:07:59 +0900

    Update Application.java

--- src/main/java/com/example/template/Application.java
+++ src/main/java/com/example/template/Application.java
@@ -3,7 +3,7 @@ package com.example.template;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
+ 
 @SpringBootApplication
 public class Application {
  
